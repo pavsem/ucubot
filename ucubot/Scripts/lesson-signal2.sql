bb58b9f13a31212c4118c3e323c1c1826c86315e
pavsem <32280258+pavsem@users.noreply.github.com>
2018-05-07 21:33:01 +0300

    Delete lesson-signal2.sql

--- ucubot/Scripts/lesson-signal2.sql
+++
@@ -1,8 +0,0 @@
-USE ucubot;
-ALTER TABLE lesson_signal DROP user_id;
-ALTER TABLE lesson_signal ADD student_id VARCHAR(50) NOT NULL;
-ALTER TABLE lesson_signal ADD 
-	CONSTRAINT fk_student_id FOREIGN KEY
-	(student_id) REFERENCES student(user_id)
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT;
